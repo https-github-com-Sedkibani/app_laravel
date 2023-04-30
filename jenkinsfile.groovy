12b609ce6d09010bf2c1ade8b4f997aabb29271f
Sedkibani <112370909+Sedkibani@users.noreply.github.com>
2023-04-30 14:19:20 +0100

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,17 +1,18 @@
 pipeline {
     agent any
-    stages ('prepare')
+    stages {
+    stage ('prepare')
         { steps    {
                 //prepare ansible-playbook 
           sh 'rm -rf ./infrastructure'
           sh ' rm -rf docker-compose.yml'
           sh 'cp -r /var/www/infrastructure/ .'
           sh 'cp -r  /var/www/infrastructure/docker/docker-compose.yml . '
-          sh 'cp -r .env.example .env      }
+          sh 'cp -r .env.example .env '     }
          }
    
  
-    stages {
+    
         stage('Checkout') {
             steps {  
                 git branch: 'main', credentialsId: 'SedkiBani', url: 'git@github.com:https-github-com-Sedkibani/app_laravel.git'
