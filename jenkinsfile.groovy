3376214ed424a480f94997eff9ce67d2f3ab3811
Sedkibani <112370909+Sedkibani@users.noreply.github.com>
2023-05-08 17:42:12 +0100

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -61,7 +61,7 @@ pipeline {
                 sh 'docker exec  php-fpm php artisan config:cache'
                 sh 'docker exec  php-fpm php artisan view:clear'
                 sh 'docker exec  php-fpm php artisan config:clear'
-              sh 'docker exec php-fpm mysql -uroot -ppassword -hdb mydatabase -e "SELECT * FROM users;"'
+        
                 // Use Ansible playbook to deploy to DigitalOcean server
                 //ansiblePlaybook(
                    // playbook: '/var/lib/jenkins/workspace/nxtya/ansible.yml',
