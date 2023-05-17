92980f9f12b9a75275fbdf00c1d9bbc9231b5032
Sedkibani <112370909+Sedkibani@users.noreply.github.com>
2023-05-17 07:55:26 +0100

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -59,7 +59,7 @@ pipeline {
                 sh 'docker exec  php-fpm php artisan config:clear'
       
             }
-    }*/
+    }
         
             stage('Deploy') {
             environment {
@@ -74,6 +74,46 @@ pipeline {
                 }
 
                 // Deploy the containers
+                sh 'COMPOSE_HTTP_TIMEOUT=480 docker-compose up -d'
+                sh 'docker exec php-fpm-blue rm -rf composer.lock vendor'
+                sh 'docker exec php-fpm-blue composer install --ignore-platform-reqs --optimize-autoloader --prefer-dist --no-scripts -o --no-dev'
+                sh 'docker exec php-fpm-blue chmod -R 0777 /var/www/html/storage'
+                sh 'docker exec php-fpm-blue php artisan key:generate'
+                sh 'docker exec php-fpm-blue php artisan config:cache'
+                sh 'docker exec php-fpm-blue php artisan view:clear'
+                sh 'docker exec php-fpm-blue php artisan config:clear'
+            }
+        }*/
+          stage('Deploy with docker-compose.yml') {
+            environment {
+                COMPOSE_FILE = 'docker-compose.yml'
+            }
+            steps {
+                // Verify if the docker-compose.yml file exists
+                script {
+                    if (!fileExists(COMPOSE_FILE)) {
+                        error "File '${COMPOSE_FILE}' does not exist."
+                    }
+                }
+
+                // Deploy using docker-compose.yml
+                sh 'COMPOSE_HTTP_TIMEOUT=480 docker-compose up -d'
+            }
+        }
+
+        stage('Deploy with docker-compose-blue.yml') {
+            environment {
+                COMPOSE_FILE = 'docker-compose-blue.yml'
+            }
+            steps {
+                // Verify if the docker-compose-blue.yml file exists
+                script {
+                    if (!fileExists(COMPOSE_FILE)) {
+                        error "File '${COMPOSE_FILE}' does not exist."
+                    }
+                }
+
+                // Deploy using docker-compose-blue.yml
                 sh 'COMPOSE_HTTP_TIMEOUT=480 docker-compose up -d'
                 sh 'docker exec php-fpm-blue rm -rf composer.lock vendor'
                 sh 'docker exec php-fpm-blue composer install --ignore-platform-reqs --optimize-autoloader --prefer-dist --no-scripts -o --no-dev'
