18e862d9fa125085b73a86bc7bcfbf3cdf2112d0
Sedkibani <112370909+Sedkibani@users.noreply.github.com>
2023-05-17 08:30:30 +0100

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -2,7 +2,7 @@ pipeline {
     agent any
      //prepare ansible-playbook 
     stages {
-    stage ('prepare')
+   /* stage ('prepare')
         { steps    {
                
           sh 'rm -rf ./infrastructure'
@@ -12,7 +12,32 @@ pipeline {
           sh 'cp -r .env.example .env '  
          sh 'ansible-playbook -i ./infrastructure/ansible/inventory/hosts.yml ./infrastructure/ansible/playbooks/install-docker.yml '
         }
-         }
+         }*/
+        
+        
+        stage('Prepare') {
+            steps {
+                script {
+                    // Backup the current docker-compose.yml
+                    sh 'mv docker-compose.yml docker-compose-blue.yml.bak'
+                    // Copy the blue version of docker-compose.yml
+                    sh 'cp -r /var/www/infrastructure/docker/docker-compose-blue.yml docker-compose.yml'
+                    
+                    // Stop the containers gracefully
+                    try {
+                        sh 'docker-compose down'
+                    } catch (err) {
+                        echo "No existing containers to stop."
+                    }
+                    
+                    // Remove any existing volumes if needed
+                    sh 'docker-compose rm -v -f'
+                    
+                    // Start the new containers
+                    sh 'docker-compose up -d'
+                }
+            }
+        }
   
        /* stage('Checkout') {
             steps {  
@@ -45,7 +70,7 @@ pipeline {
                   }
                                    }*/
      
-        stage('Deploy') {
+      /*  stage('Deploy') {
             steps {
                
                sh 'COMPOSE_HTTP_TIMEOUT=480 docker-compose up -d'
@@ -61,7 +86,50 @@ pipeline {
                 
             }
         
-    }
+    }*/
+        
+          stage('Deploy with docker-compose.yml') {
+            environment {
+                COMPOSE_FILE = 'docker-compose.yml'
+            }
+            steps {
+                // Verify if the docker-compose.yml file exists
+                script {
+                    if (!fileExists(COMPOSE_FILE)) {
+                        error "File '${COMPOSE_FILE}' does not exist."
+                    }
+                }
+
+                // Deploy using docker-compose.yml
+                sh 'COMPOSE_HTTP_TIMEOUT=480 docker-compose up -d'
+            }
+        }
+
+        stage('Deploy with docker-compose-blue.yml') {
+            environment {
+                COMPOSE_FILE = 'docker-compose-blue.yml'
+            }
+            steps {
+                // Verify if the docker-compose-blue.yml file exists
+                script {
+                    if (!fileExists(COMPOSE_FILE)) {
+                        error "File '${COMPOSE_FILE}' does not exist."
+                    }
+                }
+
+                // Deploy using docker-compose-blue.yml
+                sh 'COMPOSE_HTTP_TIMEOUT=480 docker-compose up -d'
+                sh 'docker exec php-fpm-blue rm -rf composer.lock vendor'
+                sh 'docker exec php-fpm-blue composer install --ignore-platform-reqs --optimize-autoloader --prefer-dist --no-scripts -o --no-dev'
+                sh 'docker exec php-fpm-blue chmod -R 0777 /var/www/html/storage'
+                sh 'docker exec php-fpm-blue php artisan key:generate'
+                sh 'docker exec php-fpm-blue php artisan config:cache'
+                sh 'docker exec php-fpm-blue php artisan view:clear'
+                sh 'docker exec php-fpm-blue php artisan config:clear'
+            }
+        }
+        
+        
        stage('Clean') {
             steps {
                // Stop and remove old  docker container
