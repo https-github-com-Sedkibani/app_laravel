21555f175eefddd1bcd47ad6a2fa4df15c8cfbe8
Sedkibani <112370909+Sedkibani@users.noreply.github.com>
2023-05-17 09:17:27 +0100

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -26,8 +26,6 @@ pipeline {
             // Start the new containers
             sh 'docker-compose up -d'
             
-            // Remove orphan containers
-            sh 'docker-compose down --remove-orphans'
         }
     }
 }
@@ -48,42 +46,15 @@ pipeline {
             }
         }
 
-        stage('Deploy with docker-compose.yml') {
-            environment {
-                COMPOSE_FILE = 'docker-compose.yml'
-            }
-            steps {
-                script {
-                    // Verify if the docker-compose.yml file exists
-                    if (!fileExists(COMPOSE_FILE)) {
-                        error "File '${COMPOSE_FILE}' does not exist."
-                    }
-                }
-
+        stage('Deploy') {
+    steps {
+        script {
+            if (env.COMPOSE_FILE == 'docker-compose.yml') {
                 // Deploy using docker-compose.yml
                 sh 'COMPOSE_HTTP_TIMEOUT=480 docker-compose up -d'
-
-                // Additional steps for docker-compose.yml
-                // Add your specific steps here
-            }
-        }
-
-        stage('Deploy with docker-compose-blue.yml') {
-            environment {
-                COMPOSE_FILE = 'docker-compose-blue.yml'
-            }
-            steps {
-                script {
-                    // Verify if the docker-compose-blue.yml file exists
-                    if (!fileExists(COMPOSE_FILE)) {
-                        error "File '${COMPOSE_FILE}' does not exist."
-                    }
-                }
-
+            } else if (env.COMPOSE_FILE == 'docker-compose-blue.yml') {
                 // Deploy using docker-compose-blue.yml
-                sh 'COMPOSE_HTTP_TIMEOUT=480 docker-compose up -d'
-
-                // Additional steps for docker-compose-blue.yml
+                sh 'COMPOSE_HTTP_TIMEOUT=480 docker-compose -f docker-compose-blue.yml up -d'
                 sh 'docker exec php-fpm-blue rm -rf composer.lock vendor'
                 sh 'docker exec php-fpm-blue composer install --ignore-platform-reqs --optimize-autoloader --prefer-dist --no-scripts -o --no-dev'
                 sh 'docker exec php-fpm-blue chmod -R 0777 /var/www/html/storage'
@@ -91,8 +62,15 @@ pipeline {
                 sh 'docker exec php-fpm-blue php artisan config:cache'
                 sh 'docker exec php-fpm-blue php artisan view:clear'
                 sh 'docker exec php-fpm-blue php artisan config:clear'
+                                sh 'docker-compose down --remove-orphans'
+
+            } else {
+                error "Invalid COMPOSE_FILE specified: ${env.COMPOSE_FILE}"
             }
         }
+    }
+}
+
 
         stage('Clean') {
             steps {
