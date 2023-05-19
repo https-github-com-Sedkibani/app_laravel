0b08934f078a89221f7508f281219c1fe19e7a85
Sedkibani <112370909+Sedkibani@users.noreply.github.com>
2023-05-19 15:20:00 +0100

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -20,7 +20,7 @@ pipeline {
             }
         }
 
-        stage('Build') {
+       /* stage('Build') {
             steps {
                 // Blue environment
                 sh "docker-compose -f ${BLUE_COMPOSE_FILE} build"
@@ -28,7 +28,44 @@ pipeline {
                 // Green environment
                 sh "docker-compose -f ${GREEN_COMPOSE_FILE} build"
             }
+        }*/
+            stage('Build') {
+            steps {
+                script {
+                    def blueExists = fileExists('docker-compose-blue.yml')
+                    def greenExists = fileExists('docker-compose.yml')
+                    
+                    if (blueExists && greenExists) {
+                        // Both blue and green environments exist
+                        def blueComposerExists = sh(
+                            script: 'docker-compose -f docker-compose-blue.yml exec php-fpm-blue which composer',
+                            returnStatus: true
+                        )
+                        
+                        if (blueComposerExists == 0) {
+                            // Blue environment has composer, set COMPOSE_FILE to blue
+                            COMPOSE_FILE = 'docker-compose-blue.yml'
+                        } else {
+                            // Blue environment does not have composer, set COMPOSE_FILE to green
+                            COMPOSE_FILE = 'docker-compose.yml'
+                        }
+                    } else if (blueExists) {
+                        // Only blue environment exists
+                        COMPOSE_FILE = 'docker-compose-blue.yml'
+                    } else if (greenExists) {
+                        // Only green environment exists
+                        COMPOSE_FILE = 'docker-compose.yml'
+                    } else {
+                        // No blue or green environment exists, install with green (default)
+                        COMPOSE_FILE = 'docker-compose.yml'
+                    }
+                    
+                    // Build the selected environment
+                    sh "docker-compose -f ${COMPOSE_FILE} build"
+                }
+            }
         }
+
 
         stage('Docker Login') {
             steps {
@@ -61,7 +98,7 @@ pipeline {
                         sh 'docker exec php-fpm-blue php artisan config:clear'
                     }
                 }*/
-        stage('Deploy') {
+       /* stage('Deploy') {
     steps {
         script {
             def blueExists = fileExists('docker-compose-blue.yml')
@@ -125,14 +162,53 @@ pipeline {
                 sh 'docker exec php-fpm php artisan view:clear'
                 sh 'docker exec php-fpm php artisan config:clear'
             }
-        }
-    
-    
-
-            
+        }*/
+        
+                stage('Deploy') {
+            steps {
+                script {
+                    if (COMPOSE_FILE == 'docker-compose-blue.yml') {
+                        env.PREVIOUS_BUILD = 'blue'
+                        //sh 'docker-compose -f docker-compose-blue.yml up -d'
+                            sh 'COMPOSE_HTTP_TIMEOUT=480 docker-compose-blue up -d'
+               sh 'docker exec  php-fpm-blue rm -rf composer.lock vendor'
+               sh  'docker exec  php-fpm-blue composer install --ignore-platform-reqs --optimize-autoloader --prefer-dist --no-scripts -o --no-dev'
+               sh ' docker exec  php-fpm-blue chmod -R 0777 /var/www/html/storage'
+                sh 'docker exec  php-fpm-blue php artisan key:generate'
+                sh 'docker exec  php-fpm-blue php artisan config:cache'
+                sh 'docker exec  php-fpm-blue php artisan view:clear'
+                sh 'docker exec  php-fpm-blue php artisan config:clear'
+                sh 'docker-compose -f docker-compose.yml down --remove-orphans'
+                    } else {
+                        COMPOSE_FILE = 'docker-compose.yml'
+                        env.PREVIOUS_BUILD = ''
+                        sh 'docker-compose -f docker-compose.yml up -d'
+                         sh 'COMPOSE_HTTP_TIMEOUT=480 docker-compose up -d'
+               sh 'docker exec  php-fpm rm -rf composer.lock vendor'
+               sh  'docker exec  php-fpm composer install --ignore-platform-reqs --optimize-autoloader --prefer-dist --no-scripts -o --no-dev'
+               sh ' docker exec  php-fpm chmod -R 0777 /var/www/html/storage'
+                sh 'docker exec  php-fpm php artisan key:generate'
+                sh 'docker exec  php-fpm php artisan config:cache'
+                sh 'docker exec  php-fpm php artisan view:clear'
+                sh 'docker exec  php-fpm php artisan config:clear'
+        
+                        sh 'docker-compose -f docker-compose-blue.yml down --remove-orphans'
+                    }
+                    
+                    // Additional deployment steps...
+                    // For example:
+                     sh 'docker exec php-fpm composer install --ignore-platform-reqs --optimize-autoloader --prefer-dist --no-scripts -o --no-dev'
+                     sh 'docker exec php-fpm chmod -R 0777 /var/www/html/storage'
+                     sh 'docker exec php-fpm php artisan key:generate'
+                     sh 'docker exec php-fpm php artisan config:cache'
+                     sh 'docker exec php-fpm php artisan view:clear'
+                     sh 'docker exec php-fpm php artisan config:clear'
+                }
+            }
+   
         
                 // Stop the inactive environment
-                sh "docker-compose -f ${COMPOSE_FILE} down --remove-orphans"
+               // sh "docker-compose -f ${COMPOSE_FILE} down --remove-orphans"
 
                 // Remove orphan containers (if any)
                 //sh "docker-compose -f ${COMPOSE_FILE} rm -f -v"
