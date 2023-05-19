0720b6ff65b20612a97737815095efce50c15b73
Sedkibani <112370909+Sedkibani@users.noreply.github.com>
2023-05-19 11:59:38 +0100

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -33,11 +33,11 @@ pipeline {
             }
         }
 
-        stage('Push to Docker Hub') {
+       /* stage('Push to Docker Hub') {
             steps {
                 sh 'docker push banisedki/php-fpm:latest'
                 sh 'docker push banisedki/nxtya_nginx:latest'
-            }
+            }*/
         }
 
         stage('Deploy') {
