41b4f09c34e853c7ac3b7ec0f3991cf27c029b26
Sedkibani <112370909+Sedkibani@users.noreply.github.com>
2023-05-19 18:48:18 +0100

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,54 +1,93 @@
 pipeline {
     agent any
-     //prepare ansible-playbook 
-    stages {
-    stage ('prepare')
-        { steps    {
-               
-         // sh 'rm -rf ./infrastructure'
-         // sh ' rm -rf docker-compose.yml'
-         // sh 'cp -r /var/www/infrastructure/ .'
-          //sh 'cp -r  /var/www/infrastructure/docker/docker-compose.yml . '
-        //  sh 'cp -r .env.example .env '  
-         sh 'ansible-playbook -i ./infrastructure/ansible/inventory/hosts.yml ./infrastructure/ansible/playbooks/install-docker.yml '
-        }
-         }
-  
-       /* stage('Checkout') {
-            steps {  
-                git branch: 'main', credentialsId: 'SedkiBani', url: 'git@github.com:https-github-com-Sedkibani/app_laravel.git'
-            }
-        }*/
-        
-        stage('Build') {
-            steps {
-                sh 'docker build -t banisedki/php-fpm:latest -f ./infrastructure/docker/php-fpm/Dockerfile . '
-                
-                sh 'docker build -t banisedki/nxtya_nginx:latest -f ./infrastructure/docker/nginx/Dockerfile . '
 
-			 // 'docker build -t nxtya:1.0 -f docker/Dockerfile .'
+    environment {
+        BLUE_COMPOSE_FILE = 'docker-compose-blue.yml'
+        GREEN_COMPOSE_FILE = 'docker-compose.yml'
+        COMPOSE_FILE = ''
+        PREVIOUS_BUILD = ''
+    }
+
+    stages {
+        stage('Prepare') {
+            steps {
+               //sh 'rm -rf ./infrastructure'
+               //sh 'rm -rf docker-compose.yml'
+            //    sh 'cp -r /var/www/infrastructure/ .'
+              //  sh 'cp -r /var/www/infrastructure/docker/docker-compose.yml docker-compose.yml'
+                //sh 'cp -r .env.example .env'
+                sh 'ansible-playbook -i ./infrastructure/ansible/inventory/hosts.yml ./infrastructure/ansible/playbooks/install-docker.yml'
             }
         }
- 
+
+      
+            stage('Build') {
+            steps {
+                script {
+                    def blueExists = fileExists('docker-compose-blue.yml')
+                    def greenExists = fileExists('docker-compose.yml')
+                    
+                    if (blueExists && greenExists) {
+                        // Both blue and green environments exist
+                        def blueComposerExists = sh(
+                            script: 'docker-compose -f docker-compose-blue.yml exec php-fpm-blue which composer',
+                            returnStatus: true
+                        )
+                        
+                        if (blueComposerExists == 0) {
+                            // Blue environment has composer, set COMPOSE_FILE to blue
+                            COMPOSE_FILE = 'docker-compose-blue.yml'
+                        } else {
+                            // Blue environment does not have composer, set COMPOSE_FILE to green
+                            COMPOSE_FILE = 'docker-compose.yml'
+                        }
+                    } else if (blueExists) {
+                        // Only blue environment exists
+                        COMPOSE_FILE = 'docker-compose-blue.yml'
+                    } else if (greenExists) {
+                        // Only green environment exists
+                        COMPOSE_FILE = 'docker-compose.yml'
+                    } else {
+                        // No blue or green environment exists, install with green (default)
+                        COMPOSE_FILE = 'docker-compose.yml'
+                    }
+                    
+                    // Build the selected environment
+                    sh "docker-compose -f ${COMPOSE_FILE} build"
+                }
+            }
+        }
+
+
         stage('Docker Login') {
             steps {
-                  withCredentials([string(credentialsId: 'dockerHubPwd2', variable: 'dockerHubPwd2')]) {
-               // some block
-               sh "docker login -u banisedki -p ${dockerHubPwd2}"
-                  }
-            }           
-            }      
-       stage('Push to Docker Hub') {
+                withCredentials([string(credentialsId: 'dockerHubPwd2', variable: 'dockerHubPwd2')]) {
+                    sh "docker login -u banisedki -p ${dockerHubPwd2}"
+                }
+            }
+        }
+
+   
+                stage('Deploy') {
             steps {
-                            sh 'docker push banisedki/php-fpm:latest' 
-                            sh 'docker push banisedki/nxtya_nginx:latest'
-                  }
-                                   }
-     
-        stage('Deploy') {
-            steps {
-               
-               sh 'COMPOSE_HTTP_TIMEOUT=480 docker-compose up -d'
+                script {
+                    if (COMPOSE_FILE == 'docker-compose-blue.yml') {
+                        env.PREVIOUS_BUILD = 'blue'
+                        //sh 'docker-compose -f docker-compose-blue.yml up -d'
+                            sh 'COMPOSE_HTTP_TIMEOUT=480 docker-compose-blue up -d'
+               sh 'docker exec  php-fpm-blue rm -rf composer.lock vendor'
+               sh  'docker exec  php-fpm-blue composer install --ignore-platform-reqs --optimize-autoloader --prefer-dist --no-scripts -o --no-dev'
+               sh ' docker exec  php-fpm-blue chmod -R 0777 /var/www/html/storage'
+                sh 'docker exec  php-fpm-blue php artisan key:generate'
+                sh 'docker exec  php-fpm-blue php artisan config:cache'
+                sh 'docker exec  php-fpm-blue php artisan view:clear'
+                sh 'docker exec  php-fpm-blue php artisan config:clear'
+             //   sh 'docker-compose -f docker-compose.yml down --remove-orphans'
+                    } else {
+                        COMPOSE_FILE = 'docker-compose.yml'
+                        env.PREVIOUS_BUILD = ''
+                        sh 'docker-compose -f docker-compose.yml up -d'
+                         sh 'COMPOSE_HTTP_TIMEOUT=480 docker-compose up -d'
                sh 'docker exec  php-fpm rm -rf composer.lock vendor'
                sh  'docker exec  php-fpm composer install --ignore-platform-reqs --optimize-autoloader --prefer-dist --no-scripts -o --no-dev'
                sh ' docker exec  php-fpm chmod -R 0777 /var/www/html/storage'
@@ -56,19 +95,19 @@ pipeline {
                 sh 'docker exec  php-fpm php artisan config:cache'
                 sh 'docker exec  php-fpm php artisan view:clear'
                 sh 'docker exec  php-fpm php artisan config:clear'
-        
-           sh 'docker-compose -f docker-compose.yml down --remove-orphans'
-                
-            }
-        
-    }
-       stage('Clean') {
-            steps {
-               // Stop and remove old  docker container
-               // sh 'docker stop $(docker ps -a -q)'
-                //sh 'docker rm $(docker ps -a -q)'
-          sh   'docker system prune -af --filter "until=24h" '
+                                        //sh 'docker-compose -f docker-compose-blue.yml down --remove-orphans'
+
+                  //            sh 'rm -rf ./infrastructure'
+              // sh 'rm -rf docker-compose.yml'
             }
         }
-}
+            }
+                }
+
+        stage('Clean') {
+            steps {
+                sh 'docker system prune -af --filter "until=24h"'
+            }
+        }
+    }
 }
