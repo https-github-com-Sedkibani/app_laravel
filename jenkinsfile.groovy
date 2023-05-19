366c10b4a12eb790c266b103476e934c0b3f3ad9
Sedkibani <112370909+Sedkibani@users.noreply.github.com>
2023-05-19 12:48:04 +0100

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,81 +1,74 @@
 pipeline {
     agent any
-
-    environment {
-        BLUE_COMPOSE_FILE = 'docker-compose-blue.yml'
-        GREEN_COMPOSE_FILE = 'docker-compose.yml'
-        COMPOSE_FILE = ''
-        PREVIOUS_BUILD = ''
-    }
-
+     //prepare ansible-playbook 
     stages {
-        stage('Prepare') {
-            steps {
-                sh 'rm -rf ./infrastructure'
-                sh 'rm -rf docker-compose.yml'
-                sh 'cp -r /var/www/infrastructure/ .'
-                sh 'cp -r /var/www/infrastructure/docker/docker-compose.yml docker-compose.yml'
-                sh 'cp -r .env.example .env'
-                sh 'ansible-playbook -i ./infrastructure/ansible/inventory/hosts.yml ./infrastructure/ansible/playbooks/install-docker.yml'
-            }
+    stage ('prepare')
+        { steps    {
+               
+          sh 'rm -rf ./infrastructure'
+          sh ' rm -rf docker-compose.yml'
+          sh 'cp -r /var/www/infrastructure/ .'
+          sh 'cp -r  /var/www/infrastructure/docker/docker-compose.yml . '
+          sh 'cp -r .env.example .env '  
+         sh 'ansible-playbook -i ./infrastructure/ansible/inventory/hosts.yml ./infrastructure/ansible/playbooks/install-docker.yml '
         }
-
+         }
+  
+       /* stage('Checkout') {
+            steps {  
+                git branch: 'main', credentialsId: 'SedkiBani', url: 'git@github.com:https-github-com-Sedkibani/app_laravel.git'
+            }
+        }*/
+        
         stage('Build') {
             steps {
-                // Blue environment
-                sh 'docker-compose -f ${BLUE_COMPOSE_FILE} build'
+                sh 'docker build -t banisedki/php-fpm:latest -f ./infrastructure/docker/php-fpm/Dockerfile . '
+                
+                sh 'docker build -t banisedki/nxtya_nginx:latest -f ./infrastructure/docker/nginx/Dockerfile . '
 
-                // Green environment
-                sh 'docker-compose -f ${GREEN_COMPOSE_FILE} build'
+			 // 'docker build -t nxtya:1.0 -f docker/Dockerfile .'
             }
         }
-
+ 
         stage('Docker Login') {
             steps {
-                withCredentials([string(credentialsId: 'dockerHubPwd2', variable: 'dockerHubPwd2')]) {
-                    sh "docker login -u banisedki -p ${dockerHubPwd2}"
-                }
-            }
-        }
-
+                  withCredentials([string(credentialsId: 'dockerHubPwd2', variable: 'dockerHubPwd2')]) {
+               // some block
+               sh "docker login -u banisedki -p ${dockerHubPwd2}"
+                  }
+            }           
+            }      
+       /*stage('Push to Docker Hub') {
+            steps {
+                            sh 'docker push banisedki/php-fpm:latest' 
+                            sh 'docker push banisedki/nxtya_nginx:latest'
+                  }
+                                   }
+     */
         stage('Deploy') {
             steps {
-                script {
-                    if (env.PREVIOUS_BUILD == 'blue') {
-                        COMPOSE_FILE = 'docker-compose.yml'
-                        env.PREVIOUS_BUILD = ''
-                    } else {
-                        COMPOSE_FILE = 'docker-compose-blue.yml'
-                        env.PREVIOUS_BUILD = 'blue'
-                    }
-                }
-
-                // Stop the inactive environment
-                sh "docker-compose -f ${COMPOSE_FILE} down"
-
-                // Remove orphans containers
-                sh "docker-compose -f ${COMPOSE_FILE} up -d --remove-orphans"
-
-                // Execute post-deployment tasks
-                sh 'docker exec php-fpm rm -rf composer.lock vendor'
-                sh 'docker exec php-fpm composer install --ignore-platform-reqs --optimize-autoloader --prefer-dist --no-scripts -o --no-dev'
-                sh 'docker exec php-fpm chmod -R 0777 /var/www/html/storage'
-                sh 'docker exec php-fpm php artisan key:generate'
-                sh 'docker exec php-fpm php artisan config:cache'
-                sh 'docker exec php-fpm php artisan view:clear'
-                sh 'docker exec php-fpm php artisan config:clear'
-
-                // Run health checks and tests
-                // Modify the commands below based on your specific testing needs
-                //sh 'docker exec php-fpm vendor/bin/phpunit'
-              //  sh 'docker exec webserver curl http://localhost:81/health-check'
+               
+               sh 'COMPOSE_HTTP_TIMEOUT=480 docker-compose up -d'
+               sh 'docker exec  php-fpm rm -rf composer.lock vendor'
+               sh  'docker exec  php-fpm composer install --ignore-platform-reqs --optimize-autoloader --prefer-dist --no-scripts -o --no-dev'
+               sh ' docker exec  php-fpm chmod -R 0777 /var/www/html/storage'
+                sh 'docker exec  php-fpm php artisan key:generate'
+                sh 'docker exec  php-fpm php artisan config:cache'
+                sh 'docker exec  php-fpm php artisan view:clear'
+                sh 'docker exec  php-fpm php artisan config:clear'
+        
+          
+                
             }
-        }
-
-        stage('Clean') {
-            steps {
-                sh 'docker system prune -af --filter "until=24h"'
-            }
-        }
+        
     }
+       stage('Clean') {
+            steps {
+               // Stop and remove old  docker container
+               // sh 'docker stop $(docker ps -a -q)'
+                //sh 'docker rm $(docker ps -a -q)'
+          sh   'docker system prune -af --filter "until=24h" '
+            }
+        }
+}
 }
