156b3dfff63558cce8cb5ddc2cf8d28725829eac
Sedkibani <112370909+Sedkibani@users.noreply.github.com>
2023-05-19 21:13:27 +0100

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,152 +1,51 @@
 pipeline {
     agent any
-
+    
     environment {
-        BLUE_COMPOSE_FILE = 'docker-compose-blue.yml'
-        GREEN_COMPOSE_FILE = 'docker-compose.yml'
+        BLUE_INFRA_DIR = 'infrastructure'
+        GREEN_INFRA_DIR = 'infrastructure1'
         COMPOSE_FILE = ''
         PREVIOUS_BUILD = ''
     }
 
     stages {
-        
-        
-     stage('Prepare') {
-    steps {
-        script {
-            def blueExists = fileExists('docker-compose-blue.yml')
-            def greenExists = fileExists('docker-compose.yml')
-
-            if (blueExists && greenExists) {
-                // Both blue and green environments exist
-                def blueComposerExists = sh(
-                    script: 'docker-compose -f docker-compose-blue.yml exec php-fpm-blue which composer',
-                    returnStatus: true
-                )
-
-                if (blueComposerExists == 0) {
-                    // Blue environment has composer, set COMPOSE_FILE to blue
-                    COMPOSE_FILE = 'docker-compose-blue.yml'
-                    COPY_TO_DIR = 'infrastructure'
-                    REMOVE_FROM_DIR = 'infrastructure1'
-                } else {
-                    // Blue environment does not have composer, set COMPOSE_FILE to green
-                    COMPOSE_FILE = 'docker-compose.yml'
-                    COPY_TO_DIR = 'infrastructure1'
-                    REMOVE_FROM_DIR = 'infrastructure'
-                }
-            } else if (blueExists) {
-                // Only blue environment exists
-                COMPOSE_FILE = 'docker-compose-blue.yml'
-                COPY_TO_DIR = 'infrastructure'
-                REMOVE_FROM_DIR = 'infrastructure1'
-            } else if (greenExists) {
-                // Only green environment exists
-                COMPOSE_FILE = 'docker-compose.yml'
-                COPY_TO_DIR = 'infrastructure1'
-                REMOVE_FROM_DIR = 'infrastructure'
-            } else {
-                // No blue or green environment exists, install with green (default)
-                COMPOSE_FILE = 'docker-compose.yml'
-                COPY_TO_DIR = 'infrastructure1'
-                REMOVE_FROM_DIR = 'infrastructure'
-            }
-
-            // Check the previous build and set PREVIOUS_BUILD accordingly
-            def previousBuild = currentBuild.previousBuild
-            if (previousBuild != null) {
-                def previousBuildResult = previousBuild.result
-                if (previousBuildResult == 'SUCCESS') {
-                    PREVIOUS_BUILD = 'green'
-                } else if (previousBuildResult == 'ABORTED' || previousBuildResult == 'FAILURE') {
-                    PREVIOUS_BUILD = 'blue'
-                }
-            }
-
-            sh "rm -rf ./${REMOVE_FROM_DIR}"
-            sh "rm -rf docker-compose.yml"
-            sh "cp -r /var/www/${COPY_TO_DIR}/ ."
-            sh "cp -r /var/www/${COPY_TO_DIR}/docker/${COMPOSE_FILE} docker-compose.yml"
-            sh "cp -r .env.example .env"
-            sh "ansible-playbook -i ./${COPY_TO_DIR}/ansible/inventory/hosts.yml ./${COPY_TO_DIR}/ansible/playbooks/install-docker.yml"
-        }
-    }
-}
-
-stage('Build') {
+        stage('Prepare') {
             steps {
+                sh 'rm -rf ./${BLUE_INFRA_DIR}'
+                sh 'rm -rf ./docker-compose.yml'
+                
                 script {
-                    def blueExists = fileExists('docker-compose-blue.yml')
-                    def greenExists = fileExists('docker-compose.yml')
-                    
-                    if (blueExists && greenExists) {
-                        // Both blue and green environments exist
-                        def blueComposerExists = sh(
-                            script: 'docker-compose -f docker-compose-blue.yml exec php-fpm-blue which composer',
-                            returnStatus: true
-                        )
-                        
-                        if (blueComposerExists == 0) {
-                            // Blue environment has composer, set COMPOSE_FILE to blue
-                            COMPOSE_FILE = 'docker-compose-blue.yml'
-                        } else {
-                            // Blue environment does not have composer, set COMPOSE_FILE to green
-                            COMPOSE_FILE = 'docker-compose.yml'
+                    def previousBuild = currentBuild.previousBuild
+                    if (previousBuild != null) {
+                        def previousBuildResult = previousBuild.result
+                        if (previousBuildResult == 'SUCCESS') {
+                            PREVIOUS_BUILD = 'green'
+                        } else if (previousBuildResult == 'ABORTED' || previousBuildResult == 'FAILURE') {
+                            PREVIOUS_BUILD = 'blue'
                         }
-                    } else if (blueExists) {
-                        // Only blue environment exists
-                        COMPOSE_FILE = 'docker-compose-blue.yml'
-                    } else if (greenExists) {
-                        // Only green environment exists
-                        COMPOSE_FILE = 'docker-compose.yml'
-                    } else {
-                        // No blue or green environment exists, install with green (default)
-                        COMPOSE_FILE = 'docker-compose.yml'
                     }
                     
-                    // Build the selected environment
-                    sh "docker-compose -f ${COMPOSE_FILE} build"
+                    if (PREVIOUS_BUILD == 'green') {
+                        COMPOSE_FILE = 'docker-compose.yml'
+                    } else {
+                        COMPOSE_FILE = 'docker-compose-blue.yml'
+                    }
+                    
+                    sh "cp -r /var/www/${GREEN_INFRA_DIR}/ ."
+                    sh "cp -r /var/www/${GREEN_INFRA_DIR}/docker/docker-compose.yml ."
+                    sh "cp -r .env.example .env"
+                    
+                    sh "ansible-playbook -i ./${GREEN_INFRA_DIR}/ansible/inventory/hosts.yml ./${GREEN_INFRA_DIR}/ansible/playbooks/install-docker.yml"
                 }
             }
         }
-      
-         /*   stage('Build') {
-            steps {
-                script {
-                    def blueExists = fileExists('docker-compose-blue.yml')
-                    def greenExists = fileExists('docker-compose.yml')
-                    
-                    if (blueExists && greenExists) {
-                        // Both blue and green environments exist
-                        def blueComposerExists = sh(
-                            script: 'docker-compose -f docker-compose-blue.yml exec php-fpm-blue which composer',
-                            returnStatus: true
-                        )
-                        
-                        if (blueComposerExists == 0) {
-                            // Blue environment has composer, set COMPOSE_FILE to blue
-                            COMPOSE_FILE = 'docker-compose-blue.yml'
-                        } else {
-                            // Blue environment does not have composer, set COMPOSE_FILE to green
-                            COMPOSE_FILE = 'docker-compose.yml'
-                        }
-                    } else if (blueExists) {
-                        // Only blue environment exists
-                        COMPOSE_FILE = 'docker-compose-blue.yml'
-                    } else if (greenExists) {
-                        // Only green environment exists
-                        COMPOSE_FILE = 'docker-compose.yml'
-                    } else {
-                        // No blue or green environment exists, install with green (default)
-                        COMPOSE_FILE = 'docker-compose.yml'
-                    }
-                    
-                    // Build the selected environment
-                    sh "docker-compose -f ${COMPOSE_FILE} build"
-                }
-            }
-        }*/
 
+        stage('Build') {
+            steps {
+                sh "docker build -t banisedki/php-fpm:latest -f ./${GREEN_INFRA_DIR}/docker/php-fpm/Dockerfile ."
+                sh "docker build -t banisedki/nxtya_nginx:latest -f ./${GREEN_INFRA_DIR}/docker/nginx/Dockerfile ."
+            }
+        }
 
         stage('Docker Login') {
             steps {
@@ -156,42 +55,25 @@ stage('Build') {
             }
         }
 
-   
-                stage('Deploy') {
+        stage('Push to Docker Hub') {
             steps {
-                script {
-                    if (COMPOSE_FILE == 'docker-compose-blue.yml') {
-                        env.PREVIOUS_BUILD = 'blue'
-                        //sh 'docker-compose -f docker-compose-blue.yml up -d'
-                            sh 'COMPOSE_HTTP_TIMEOUT=480 docker-compose-blue up -d'
-               sh 'docker exec  php-fpm-blue rm -rf composer.lock vendor'
-               sh  'docker exec  php-fpm-blue composer install --ignore-platform-reqs --optimize-autoloader --prefer-dist --no-scripts -o --no-dev'
-               sh ' docker exec  php-fpm-blue chmod -R 0777 /var/www/html/storage'
-                sh 'docker exec  php-fpm-blue php artisan key:generate'
-                sh 'docker exec  php-fpm-blue php artisan config:cache'
-                sh 'docker exec  php-fpm-blue php artisan view:clear'
-                sh 'docker exec  php-fpm-blue php artisan config:clear'
-             //   sh 'docker-compose -f docker-compose.yml down --remove-orphans'
-                    } else {
-                        COMPOSE_FILE = 'docker-compose.yml'
-                        env.PREVIOUS_BUILD = ''
-                        sh 'docker-compose -f docker-compose.yml up -d'
-                         sh 'COMPOSE_HTTP_TIMEOUT=480 docker-compose up -d'
-               sh 'docker exec  php-fpm rm -rf composer.lock vendor'
-               sh  'docker exec  php-fpm composer install --ignore-platform-reqs --optimize-autoloader --prefer-dist --no-scripts -o --no-dev'
-               sh ' docker exec  php-fpm chmod -R 0777 /var/www/html/storage'
-                sh 'docker exec  php-fpm php artisan key:generate'
-                sh 'docker exec  php-fpm php artisan config:cache'
-                sh 'docker exec  php-fpm php artisan view:clear'
-                sh 'docker exec  php-fpm php artisan config:clear'
-                                        //sh 'docker-compose -f docker-compose-blue.yml down --remove-orphans'
-
-                  //            sh 'rm -rf ./infrastructure'
-              // sh 'rm -rf docker-compose.yml'
+                sh 'docker push banisedki/php-fpm:latest' 
+                sh 'docker push banisedki/nxtya_nginx:latest'
             }
         }
+
+        stage('Deploy') {
+            steps {
+                sh "COMPOSE_HTTP_TIMEOUT=480 docker-compose -f ${COMPOSE_FILE} up -d"
+                sh "docker exec php-fpm rm -rf composer.lock vendor"
+                sh "docker exec php-fpm composer install --ignore-platform-reqs --optimize-autoloader --prefer-dist --no-scripts -o --no-dev"
+                sh "docker exec php-fpm chmod -R 0777 /var/www/html/storage"
+                sh "docker exec php-fpm php artisan key:generate"
+                sh "docker exec php-fpm php artisan config:cache"
+                sh "docker exec php-fpm php artisan view:clear"
+                sh "docker exec php-fpm php artisan config:clear"
             }
-                }
+        }
 
         stage('Clean') {
             steps {
