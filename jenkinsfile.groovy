655caf200da5722f7c9f2f176ee72a6ea67d7ab5
Sedkibani <112370909+Sedkibani@users.noreply.github.com>
2023-05-22 13:33:49 -0700

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -5,10 +5,10 @@ pipeline {
     stage ('prepare')
         { steps    {
                
-          //sh 'rm -rf ./infrastructure'
-          //sh ' rm -rf docker-compose.yml'
-          //sh 'cp -r /var/www/infrastructure/ .'
-          //sh 'cp -r  /var/www/infrastructure/docker/docker-compose.yml . '
+          sh 'rm -rf ./infrastructure'
+          sh ' rm -rf docker-compose.yml'
+          sh 'cp -r /var/www/infrastructure/ .'
+          sh 'cp -r  /var/www/infrastructure/docker/docker-compose.yml . '
          sh 'cp -r .env.example .env '  
          sh 'ansible-playbook -i ./infrastructure/ansible/inventory/hosts.yml ./infrastructure/ansible/playbooks/install-docker.yml '
         }
